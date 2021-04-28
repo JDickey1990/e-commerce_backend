f151b1746aaed3fd7c7c8741efc7eeda239ac1ce
jmd <jordan.michael.dickey@gmail.com>
2021-04-27 18:56:22 -0600

    Namespacing controllers and routes and setting up Cors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  resources :products
-  resources :orders
-  resources :users
+
+namespace :api do 
+  namespace :v1 do 
+    resources :products
+    resources :orders
+    resources :users
+  end
+end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+#fetch('http://localhost:3000/api/vi/')
