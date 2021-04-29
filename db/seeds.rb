c7e5718c7b2d8a5fa62829602a02e3beb6175117
jmd <jordan.michael.dickey@gmail.com>
2021-04-29 16:13:55 -0600

    creating  seed file for the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,121 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Product.destroy_all
+OrdersProduct.destroy_all
+Order.destroy_all
+
+storeProducts = [
+    {
+      id: 1,
+      model: "5374P-001 - Platinum",
+      img: "img/Product-1_5374P_001.jpg",
+      price: 55000 ,
+      company: "Patek Philippe",
+      collection: "Grand Complications",
+      info: "The Grand Complication with a minute repeater and a perpetual calendar sports a new face and a platinum case with gentle cambers and rounds emphasized by the concave bezel and the satin-finished, recessed flanks. The black dial in genuine enamel carries applied Breguet numerals and recessed counters.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 2,
+      model: "5208R-001 - Rose Gold",
+      img: "img/Product-2_5208R_001.jpg",
+      price: 63000,
+      company: "Patek Philippe",
+      collection: "Grand Complications",
+      info:
+        "Patek Philippe has reinterpreted its Reference 5208 Grand Complication by housing it for the first time in a rose-gold case, paired with an ebony black sunburst dial. The 719-part movement, combining a minute repeater, a monopusher chronograph and an instantaneous perpetual calendar with aperture displays, is one of the rare ultra-complicated calibers to be self-winding. It can be admired through a sapphire-crystal case back.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 3,
+      model: "5270J-001 - Yellow Gold",
+      img: "img/Product-3_5270J_001.jpg",
+      price: 176240,
+      company: "Patek Philippe",
+      collection: "Grand Complications",
+      info:
+        "As the heir of a grand Patek Philippe classic, the Ref. 5270 Chronograph with a perpetual calendar is now available in yellow gold for the first time – an elegant outfit for this timeless watch with a concave bezel and fluted lugs. The silvery opaline dial is superbly legible. The caliber CH 29-535 PS Q movement combines traditional design with six patented innovations and an extremely slender calendar mechanism.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 4,
+      model: "5270P-001 - Platinum",
+      img: "img/Product-4_5270P_001.jpg",
+      price: 205810,
+      company: "Patek Philippe",
+      collection: "Grand Complications",
+      info:
+        "Heir to a great Patek Philippe classic launched in 1941, the Reference 5270 perpetual calendar chronograph is now available in platinum for the first time. Its golden opaline dial, displaying the day and the month in a double aperture at 12 o’clock and an analog date at 6 o’clock, combines a hint of vintage elegance with great legibility. The chronograph movement, with its classic architecture – horizontal clutch, column wheel and manual winding – features six patented innovations.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 5,
+      model: "5271P-001 - Platinum",
+      img: "img/Product-5_5271P_001.jpg",
+      price: 289790,
+      company: "Patek Philippe",
+      collection: "Grand Complications",
+      info:
+        "The chronograph has always featured prominently in the vast range of complications in which Patek Philippe has demonstrated its virtuosity. For over 175 years, the manufacture has created a wealth of different models, many of which count among the timepieces most admired and sought after by connoisseurs and collectors.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 6,
+      model: "5303R-001 - White Gold",
+      img: "img/Product-6_5303R_001.jpg",
+      price: 423500,
+      company: "Patek Philippe",
+      collection: "Grand Complications",
+      info:
+        "This new grand complication was created explicitly for connoisseurs of striking watches. It stands out with its open architecture that allows the minute repeater mechanism including the hammers and gongs to be admired in action while the watch is being worn on the wrist. The aesthetic appeal of the manually wound caliber R TO 27 PS was subtly reworked to highlight its full beauty. The rose-gold case features white-gold intarsias engraved with foliage motifs.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 7,
+      model: "6102P-001 - Platinum",
+      img: "img/Product-7_6102P_001.jpg",
+      price: 334730,
+      company: "Patek Philippe",
+      collection: "Grand Complications",
+      info:
+        "Highlighting Patek Philippe’s great tradition of astronomical watches, the Celestial devotes its dial to a rotating chart of the heavenly bodies. At any time, its owner may admire the exact configuration of the nocturnal sky in the northern hemisphere, with the apparent movement of the stars and the phases and orbit of the moon. Two skeletonized hands point to the hours and minutes of mean solar time. An Ellipse, deposited on the underside of the sapphire glass, frames the portion of the sky visible from Geneva and all other cities located at the same latitude.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 8,
+      model: "5316P-001 - Platinum",
+      img: "img/Product-8_5316P_001.jpg",
+      price: 92260,
+      company: "Patek Philippe",
+      collection: "Grand Complications",
+      info:
+        "Patek Philippe has redesigned its famous Grand Complication with a minute repeater, a tourbillon, and a perpetual calendar with a retrograde date. It sports a new platinum case with a two-tier bezel and lugs as well as a black enamel dial. The retrograde date is indicated by an unusual hand with a diamond-shaped tip.",
+      inCart: false,
+      count: 0,
+      total: 0
+    }
+
+  ]
+  
+storeProducts.each do |product|
+    Product.create(product)
+end
+    
+   
