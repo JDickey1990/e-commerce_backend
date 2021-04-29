3ef4566ccbdfa31b18e9386b1491a1a0dfa20037
jmd <jordan.michael.dickey@gmail.com>
2021-04-29 16:10:25 -0600

    configuring the orders controller wih strong params and an index and create action

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,2 +1,47 @@
+require 'pry'
 class Api::V1::OrdersController < ApplicationController
+
+    def create
+        order = Order.create(order_params)
+        products = params[:products_attributes]
+        
+      
+        products.each do |prod|
+            product =Product.find_by(id: prod[:id])
+            order.products << product
+        end
+
+        render json: order, include: [:products] 
+    end
+
+    def index
+        orders = Order.all
+        render json: orders, include: [:products] 
+    end
+
+    private
+
+    def order_params
+        params.require(:order).permit(
+                :subtotal,
+                :tax,
+                :total,
+                products_attributes:[
+                        :id,
+                        :model,
+                        :img,
+                        :price,
+                        :company,
+                        :collection,
+                        :info,
+                        :inCart,
+                        :count,
+                        :total
+                ] 
+            
+            )
+    end
+
+ 
 end
+
